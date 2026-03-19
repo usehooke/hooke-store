44bf41d1ff34e03b5e658f11bfa1129384336c9e
usehooke <nandof83@gmail.com>
2026-03-19 14:24:51 -0300

    fix(admin): aumenta limite do uploadthing para 16MB

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -4,7 +4,7 @@ const f = createUploadthing();
 
 export const ourFileRouter = {
     // Define as many FileRoutes as you like, each with a unique routeSlug
-    imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    imageUploader: f({ image: { maxFileSize: "16MB", maxFileCount: 1 } })
         .onUploadComplete(async ({ file }) => {
             // This code RUNS ON YOUR SERVER after upload
             console.log("Upload complete for url:", file.url);
